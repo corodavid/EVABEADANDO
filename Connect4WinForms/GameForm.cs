38f1636aa42606f73c3ca47a781f662f6d1c1236
Coroianu Dávid <coroianudavid@gmail.com>
2025-10-10 11:03:09 +0200

    A konstruktorokat atalakitottam

--- Connect4WinForms/GameForm.cs
+++ Connect4WinForms/GameForm.cs
@@ -10,8 +10,7 @@ namespace Connect4WinForms
     public partial class GameForm : Form
     {
         private GameModel _gameModel;
-        private Label[,] _labelGrid;
-        private Button _buttons;
+        private Button[,] _buttonGrid;
 
 
 
@@ -20,6 +19,32 @@ namespace Connect4WinForms
             InitializeComponent();
 
             IGameTableDataAccess _dataAccess = new GameTableDataAccess();
+            _gameModel = new GameModel(_dataAccess);
+
+            _gameModel.OnBoardChanged += new EventHandler<Connect4FieldEventArgs>(Game_BoardChanged);
+            _gameModel.OnGameEnded += new EventHandler<Connect4EventArgs>(Game_Ended);
+            _gameModel.OnTimerTick += new Eventhandler<EventArgs>(Game_TimerTick);
+            
+            SetupMenus();
+        }
+
+         
+
+        private void SetupTable(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                        [i, j].Text = _model[i,j] == FieldStatus.NONE
+                            ? String.Empty
+                            : _model[i, j].ToString();
+                        
+                        _buttonGrid[i, j].Enabled = true;
+                        _buttonGrid[i, j].BackColor = Color.White;
+                }
+            }
+
         }
     }
 }
